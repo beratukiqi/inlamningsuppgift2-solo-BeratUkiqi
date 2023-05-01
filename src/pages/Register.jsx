aba08ee8e9571a72b9f07ae9c4a791572adf4f92
beratukiqi <berat.u@live.se>
2023-05-01 08:31:24 +0200

    Add new player (+1) functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,28 @@ import Header from "../components/Header";
 import PrimaryButton from "../components/PrimaryButton";
 import SecondaryButton from "../components/SecondaryButton";
 import PlayerRegister from "../components/PlayerRegister";
+import { changeNoOfPlayers } from "../app/gameSettingsSlice";
+import { generatePlayer } from "../app/playersSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Register() {
+    const dispatch = useDispatch();
+    const gameSettings = useSelector((state) => state.gameSettings);
+    const addNewPlayer = () => {
+        dispatch(changeNoOfPlayers(gameSettings.noOfPlayers + 1));
+
+        const newPlayer = {
+            id: "Player " + gameSettings.noOfPlayers,
+            name: "Player " + gameSettings.noOfPlayers,
+            bgColor: "#A5E9B4",
+            points: 0,
+            pointsToAdd: 0,
+            pointsHistory: [0],
+        };
+
+        dispatch(generatePlayer(newPlayer));
+    };
     return (
         <main>
             <Header
@@ -17,7 +37,7 @@ function Register() {
                 title={"Registration is open!"}
                 renderContent={() => <PlayerRegister />}
             />
-            <SecondaryButton title={"Add new player"} />
+            <SecondaryButton title={"Add new player"} action={addNewPlayer} />
             <PrimaryButton title={"Players are ready"} path={"/overview"} />
         </main>
     );
