936be06721c2600c802dae6761f195637f3da5f5
beratukiqi <berat.u@live.se>
2023-05-03 15:01:13 +0200

    Handling for when a player looses

--- src/components/PlayerCardSmall.jsx
+++ src/components/PlayerCardSmall.jsx
@@ -3,16 +3,22 @@ import { useSelector } from "react-redux";
 
 function PlayerCardSmall({ player, dangerZone, bgColor }) {
     const gameSettings = useSelector((state) => state.gameSettings);
+    const hasPassedMAX = gameSettings.maxPoints - player.points <= 0;
 
     return (
         <article
             className={style.playerCardSmall}
-            style={{ backgroundColor: `${bgColor}` }}
+            style={{ backgroundColor: hasPassedMAX ? "#ff3f3f" : `${bgColor}` }}
         >
             <p>{player.name}</p>
             <p>
                 {dangerZone
-                    ? gameSettings.maxPoints - player.points + " points left"
+                    ? gameSettings.maxPoints - player.points <= 0
+                        ? Math.abs(gameSettings.maxPoints - player.points) +
+                          " points above"
+                        : gameSettings.maxPoints -
+                          player.points +
+                          " points left"
                     : player.points}
             </p>
         </article>
